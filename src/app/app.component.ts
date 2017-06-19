f6a0eeff5b0de5d80cf565e3fcdfeacac3460e79
trwolfe13 <trwolfe13@gmail.com>
2017-06-19 16:08:50 +0100

    Add clear button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,16 @@ export class AppComponent {
   onGenerateClick() {
     this.character = this.generator.generateCharacter();
   };
+
+  onClearClick() {
+    this.config.background = null;
+    this.config.class = null;
+    this.config.classArchetype = null;
+    this.config.maxLevel = 5;
+    this.config.minLevel = 1;
+    this.config.primaryAbility = null;
+    this.config.race = null;
+    this.config.secondaryAbility = null;
+    this.config.subrace = null;
+  }
 };
