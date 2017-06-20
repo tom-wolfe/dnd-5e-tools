88de4be59f0bc81c0bc1cd54b196094f26bf1090
trwolfe13 <trwolfe13@gmail.com>
2017-06-20 11:50:52 +0100

    Attempt automated Azure deployment.

---
+++ deploy.cmd
@@ -0,0 +1,86 @@
+echo on
+
+:: ----------------------
+:: KUDU Deployment Script
+:: ----------------------
+
+:: Prerequisites
+:: -------------
+
+:: Verify node.js installed
+where node 2>nul >nul
+IF %ERRORLEVEL% NEQ 0 (
+  echo Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.
+  goto error
+)
+
+:: Setup
+:: -----
+
+setlocal enabledelayedexpansion
+
+SET ARTIFACTS=%~dp0%artifacts
+
+:: IF NOT DEFINED DEPLOYMENT_SOURCE (
+::  SET DEPLOYMENT_SOURCE=%~dp0%.
+:: )
+
+:: IF NOT DEFINED DEPLOYMENT_TARGET (
+::  SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot
+:: )
+
+SET DEPLOYMENT_SOURCE=\home\site\repository
+SET DEPLOYMENT_TARGET=\home\site\wwwroot
+
+IF NOT DEFINED NEXT_MANIFEST_PATH (
+  SET NEXT_MANIFEST_PATH=%ARTIFACTS%\manifest
+
+  IF NOT DEFINED PREVIOUS_MANIFEST_PATH (
+    SET PREVIOUS_MANIFEST_PATH=%ARTIFACTS%\manifest
+  )
+)
+
+IF NOT DEFINED KUDU_SYNC_COMMAND (
+  :: Install kudu sync
+  echo Installing Kudu Sync
+  call npm install kudusync -g --silent
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  :: Locally just running "kuduSync" would also work
+  SET KUDU_SYNC_COMMAND=node "%appdata%\npm\node_modules\kuduSync\bin\kuduSync"
+)
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Deployment
+:: ----------
+
+echo Handling node.js deployment.
+
+:: 1. KuduSync
+echo Kudu Sync from "%DEPLOYMENT_SOURCE%" to "%DEPLOYMENT_TARGET%"
+call %KUDU_SYNC_COMMAND% -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.deployment;deploy.cmd" 2>nul
+IF !ERRORLEVEL! NEQ 0 goto error
+
+:: 2. Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\App_Data\jobs\continuous\pnwebjob\package.json" (
+  pushd %DEPLOYMENT_TARGET%\App_Data\jobs\continuous\pnwebjob
+  echo Starting npm install
+  call npm install
+  :: call tar xvfz node_modules.tgz
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 3 Build using NG.
+"%appdata%\npm\node_modules\.bin\ng" build --prod
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+goto end
+
+:error
+echo An error has occured during web site deployment.
+exit /b 1
+
+:end
+echo Finished successfully.
