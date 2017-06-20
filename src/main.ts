8f6ae9520365e7f5af9687bf27b1385d720eb890
trwolfe13 <trwolfe13@gmail.com>
2017-06-20 12:24:17 +0100

    Fix references. Remove console logging.

--- src/main.ts
+++ src/main.ts
@@ -8,28 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-import * as Markov from "./markov";
-
-const chain = new Markov.MarkovChain<string>(2);
-
-const names = [
-  "Wilcome",
-  "Waldolanus",
-  "Engilbert",
-  "Cyr",
-  "Leodegar",
-  "Gilbert",
-  "Arbitio",
-  "Merimas",
-  "Doderic"
-]
-
-names.forEach(name => {
-  chain.add(name.split(""));
-});
-
-for (let x = 0; x < 10; x++) {
-  console.log(chain.walk().join(""));
-}
-
 platformBrowserDynamic().bootstrapModule(AppModule);
