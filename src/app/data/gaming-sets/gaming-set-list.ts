a74afbc546bfe7a6f9a3446e378197ce0d483836
Thomas Wolfe <Thomas Wolfe>
2017-06-29 14:32:48 +0100

    Improve gaming proficiency terminology.

--- src/app/data/gaming-sets/gaming-set-list.ts
+++ src/app/data/gaming-sets/gaming-set-list.ts
@@ -2,14 +2,14 @@ import { Item } from "app/models/equipment";
 
 export const GamingSetList: Item[] = [];
 
-export const Dice: Item = new Item({ name: "Dice set" });
+export const Dice: Item = new Item({ name: "Dice" });
 GamingSetList.push(Dice);
 
-export const DragonChess: Item = new Item({ name: "Dragon chess set" });
+export const DragonChess: Item = new Item({ name: "Dragon chess" });
 GamingSetList.push(DragonChess);
 
-export const PlayingCards: Item = new Item({ name: "Playing card set" });
+export const PlayingCards: Item = new Item({ name: "Playing cards" });
 GamingSetList.push(PlayingCards);
 
-export const ThreeDragonAnte: Item = new Item({ name: "Three-Dragon Ante set" });
+export const ThreeDragonAnte: Item = new Item({ name: "Three-Dragon Ante" });
 GamingSetList.push(ThreeDragonAnte);
