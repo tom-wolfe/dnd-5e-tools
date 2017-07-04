0294c934654bb7d05c8c6c15eb16e1beff9a9c2d
Tom Wolfe <trw@prospectsoft.com>
2017-07-04 15:55:32 +0100

    Remove obsolete code.

--- src/app/shared/descriptors/character-descriptor.ts
+++ src/app/shared/descriptors/character-descriptor.ts
@@ -4,12 +4,4 @@ import { Descriptor } from "./descriptor";
 
 export abstract class CharacterDescriptor extends Descriptor<Characters.Character> {
     abstract describe(object: Characters.Character): string;
-
-    getBand(value: number, lower: number, upper: number, bands: number): number {
-        const bandSize = (upper - lower) / bands;
-
-        if (value < lower) { return 0; }
-        if (value > upper) { return bands + 1; }
-        return Math.max(Math.ceil((value - lower) / bandSize), 1);
-    }
 };
