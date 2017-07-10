acf6544bff10c6ec43ec3cab28c96c36e850b89a
Tom Wolfe <trwolfe13@gmail.com>
2017-07-10 21:00:43 +0100

    Improve responsive styling.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,27 @@
 import { async, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
 import { AppComponent } from "./app.component";
+
+import { CharacterGeneratorModule } from "./character-generator/character-generator.module";
+import { CoreModule } from "./core/core.module";
+import { InitiativeTrackerModule } from "./initiative-tracker/initiative-tracker.module";
+import { ItemGeneratorModule } from "./item-generator/item-generator.module";
+import { NameGeneratorModule } from "./name-generator/name-generator.module";
+import { SharedModule } from "./shared/shared.module";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        SharedModule,
+        CharacterGeneratorModule,
+        NameGeneratorModule,
+        ItemGeneratorModule,
+        InitiativeTrackerModule,
+        CoreModule,
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -17,16 +34,9 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title "app"`, async(() => {
+  it(`should have as title "D&D 5th Edition Tools"`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("app");
-  }));
-
-  it("should render title in a h1 tag", async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain("Welcome to app!!");
+    expect(app.title).toEqual("D&D 5th Edition Tools");
   }));
 });
